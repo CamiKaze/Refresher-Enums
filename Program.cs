1a88e38bd2425914d771b272e6b7a16bde0523b9
CamiKaze <cameronsmith63@gmail.com>
2020-03-12 10:07:37 +0200

    Added my project

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Enums
+{
+    public enum ShippingMethod
+    {
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // These are two ways of converting an Enum to and from an integer.
+
+            var method = ShippingMethod.Express;
+            // Internally, Enum is an int. So we can cast that to an int if we want the numeric value.
+            System.Console.WriteLine((int)method);
+
+            /* Let's say that we receive a value from a different system and we need to convert this value to a ShippingMethod in the application.
+            We can use casting to convert the value to a ShippMethod. */
+            var methodId = 3;
+            System.Console.WriteLine((ShippingMethod)methodId);
+
+            // There are times when we need to convert an Enum to a string.
+            // Or, get a string and convert it to an Enum.
+
+            // If we want the string of a value, we can convert the method to a string.
+            System.Console.WriteLine(method.ToString());
+
+            // What if we have a string, and we want to convert that to an Enum?
+            // Let's say that we have that string stored somewhere in a database, or in another system.
+            // we use parsing to convert the string to a different type.
+            var methodName = "Express";
+             // The first value in the Enum is a target, the second is a value.
+             // This returns an object, so we need to cast the object to a ShippingMethod.
+            var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
+        }
+    }
+}
